acf0149c957af0fd5997a846b796d6b92789de5a
Horuz <ortizgd30@gmail.com>
2023-01-28 23:29:02 -0600

    excercise1.py is finished

---
+++ excercise1.py
@@ -0,0 +1,13 @@
+user_number = int(input("Introduzca un numero, por favor: "))
+
+divi3 = user_number / 3
+resultado_true_false = divi3 > 2
+
+
+# user_number_int = int(user_number)
+# print(type(user_number_int))
+print(user_number)
+print(type(user_number))
+print(divi3)
+print(resultado_true_false)
+
