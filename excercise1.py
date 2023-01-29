d81e5054b387c0c3ac7f52685a73fc7a1ae6715e
Horuz <ortizgd30@gmail.com>
2023-01-29 13:17:37 -0600

    change example

--- excercise1.py
+++ excercise1.py
@@ -1,15 +1,15 @@
-print("Welcome")
-user_number = int(input("Introduzca un numero, por favor: "))
+if __name__ == "__main__":
+    print("Welcome")
+    user_number = int(input("Introduzca un numero, por favor: "))
 
-divi3 = user_number / 3
-resultado_true_false = divi3 > 2
+    divi3 = user_number / 3
+    resultado_true_false = divi3 > 2
 
+    # user_number_int = int(user_number)
+    # print(type(user_number_int))
+    print(user_number)
+    print(type(user_number))
+    print(divi3)
+    print(resultado_true_false)
 
-# user_number_int = int(user_number)
-# print(type(user_number_int))
-print(user_number)
-print(type(user_number))
-print(divi3)
-print(resultado_true_false)
-
-print("Bye")
+    print("Bye")
